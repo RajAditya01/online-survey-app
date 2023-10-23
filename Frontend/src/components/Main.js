d54abbf522b2bb6458ab988ebe6bb668cf42c371
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-10-23 17:23:34 +0530

    backend

--- Frontend/src/components/Main.js
+++ Frontend/src/components/Main.js
@@ -9,33 +9,43 @@ export default function Main() {
     const inputRef = useRef(null)
     const dispatch = useDispatch()
 
-
-    function startQuiz(){
-        if(inputRef.current?.value){
+    function startQuiz() {
+        if (inputRef.current?.value) {
             dispatch(setUserId(inputRef.current?.value))
         }
     }
 
-  return (
-    <div className='container'>
-        <h1 className='title text-light'>Quiz Application</h1>
+    return (
+        <div className='container'>
+            <h1 className='title text-light'>Online Survey Application</h1>
 
-        <ol>
-            <li>You will be asked 10 questions one after another.</li>
-            <li>10 points is awarded for the correct answer.</li>
-            <li>Each question has three options. You can choose only one options.</li>
-            <li>You can review and change answers before the quiz finish.</li>
-            <li>The result will be declared at the end of the quiz.</li>
-        </ol>
+            <p></p>
 
-        <form id="form">
-            <input ref={inputRef} className="userid" type="text" placeholder='Username*' />
-        </form>
+            <section className='paragraph'> 
+            <p>
+            Welcome to the survey!
+            </p>
 
-        <div className='start'>
-            <Link className='btn' to={'quiz'} onClick={startQuiz}>Start Quiz</Link>
+            <p>
+                Here are the rules:
+            </p>
+            </section>
+            <ol>
+                <li>You will be asked 10 questions one after another.</li>
+                <li>10 points will be awarded for each correct answer.</li>
+                <li>Each question has three options, and you can choose only one option.</li>
+                <li>You can review and change your answers before the survey finishes.</li>
+                <li>The result will be declared at the end of the survey.</li>
+            </ol>
+            <p className='paragraph' >Thank you for participating!</p>
+
+            <form id="form">
+                <input ref={inputRef} className="userid" type="text" placeholder='Username*' />
+            </form>
+
+            <div className='start'>
+                <Link className='btn' to={'quiz'} onClick={startQuiz}>Start Survey</Link>
+            </div>
         </div>
-
-    </div>
-  )
+    );
 }
