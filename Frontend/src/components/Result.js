1885dacff1817c95fe16e6fcecc15f89d086c535
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-10-27 19:02:01 +0530

    .

--- Frontend/src/components/Result.js
+++ Frontend/src/components/Result.js
@@ -38,7 +38,7 @@ export default function Result() {
 
   return (
     <div className='container'>
-        <h1 className='title text-light'>Quiz Application</h1>
+        <h1 className='title text-light'>Online Survey Application</h1>
 
         <div className='result flex-center'>
             <div className='flex'>
